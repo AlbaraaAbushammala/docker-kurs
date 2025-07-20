1e335d102b15a1dba8d9bdf9b857a6fdd0346d51
Albaraa Abushammala <76408798+AlbaraaAbushammala@users.noreply.github.com>
2025-07-20 23:07:48 +0200

    Docker

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// init app
+const PORT = process.env.PORT || 4000; // هيك معناه ي نستخدم ٤٠٠٠ كديفولت بورت او راح استخدم البورت الي برسله
+// طيب كيف هنرسله مع الكونتينر اذا كنا نستخدم الدوكر فايل بدون الدوكر كمبووز هذي الطريقه الاولى
+// ENV PORT = 4000 اول شي نكتب هيك 
+// EXPOSE $PORT وبعدها نكتب جنب كلمه
+// الطريقه الثانيه انه نرسله مع الرن كود عبر التيرمنال 
+// docker run --name express-node-app-container -v $(pwd)/src:/app/src:ro --env PORT=4000 -d -p 4000:4000 express-node-app
+// --env PORT=4000 بالطريقه هذي نرسلها مع الكود ونقدر نضيف اكثر من انف كمان 
+// اللحين لو دخلنا على الكونتينر بعد ما سوينا للكونتينر رن
+// راح تستعرضهم لي كلهم printenv الي جوا الكونتينر  بكتب الامر هذا في التريمنال  Environment Variables عشان استعرض كل 
+// مو منطقي اكتبهم كلهم الحل هو  Environment Variables طيب مثلا لو كان عندي اكثر من 
+// حقاتي  Environment Variables واكتب جواه .env اني اسوي ملف اسمه 
+// الامر يتغير واكتب انه يروح ياخذهم من ملف الانف
+// docker run --name express-node-app-container -v $(pwd)/src:/app/src:ro --env-file ./.env -d -p 4000:4000 express-node-app
+
+// طبعا كل الشرح هذا بدون ما نستخدم دوكر كمبوز معاه الوضع شوي اسهل شوف التكمله في ملف الكمبوز 
+
+
+const app = express();
+
+
+app.get('/', (req, res) => res.send('<h1>Hello Albaraa sss</h1>'));
+
+
+app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`));
