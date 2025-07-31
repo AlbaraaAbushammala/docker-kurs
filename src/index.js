c7e35f02d2e498678e67a08a2fe9b042eeb80980
Albaraa Abushammala <76408798+AlbaraaAbushammala@users.noreply.github.com>
2025-07-31 23:53:47 +0200

    mm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // init app
 const PORT = process.env.PORT || 4000; // هيك معناه ي نستخدم ٤٠٠٠ كديفولت بورت او راح استخدم البورت الي برسله
@@ -19,6 +20,14 @@ const PORT = process.env.PORT || 4000; // هيك معناه ي نستخدم ٤٠
 
 
 const app = express();
+
+// connect db
+const DB_USER = "root"
+const DB_PASSWORD = "example"
+const DB_PORT = 27017
+const DB_HOST = "mongo"
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+mongoose.connect(URI).then(()=> console.log("connect to db...")).catch((err) => console.log(`failed to connect to db :`, err));
 
 
 app.get('/', (req, res) => res.send('<h1>Hello Albaraa sss</h1>'));
